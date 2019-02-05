ca36516dd8351ef34dca9f03d42cc324a567e7a6
Krzysztof Szypulski <krzychu700@gmail.com>
2019-02-05 16:35:30 +0100

    Updates

---
+++ precache-manifest.49ce77b93bbf62b606a39dd66254d71f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a4c97e913adeac98b80",
+    "url": "/shop/static/js/runtime~main.3a4c97e9.js"
+  },
+  {
+    "revision": "bbd185060d71677fc192",
+    "url": "/shop/static/js/main.bbd18506.chunk.js"
+  },
+  {
+    "revision": "7dbd364e7a42f7b87154",
+    "url": "/shop/static/js/1.7dbd364e.chunk.js"
+  },
+  {
+    "revision": "bbd185060d71677fc192",
+    "url": "/shop/static/css/main.ee4918e3.chunk.css"
+  },
+  {
+    "revision": "48290c8ac3b5dd77f3c737ee6a4e5b2c",
+    "url": "/shop/index.html"
+  }
+];
